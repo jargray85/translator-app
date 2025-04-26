4f110fe4af34173329dd3fd499485509f2841782
James Gray <jamesrjgray@gmail.com>
2025-04-25 19:07:22 -0500

    added netlify and render config files and updated API endpoints to use local in dev and env variables in prod

--- frontend/src/components/Translator.tsx
+++ frontend/src/components/Translator.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { API_BASE_URL } from '../config';
 import './Translator.css';
 
 interface Language {
@@ -38,7 +39,7 @@ const Translator: React.FC = () => {
     setError(null);
 
     try {
-      const response = await fetch('http://localhost:3000/api/translate', {
+      const response = await fetch(`${API_BASE_URL}/api/translate`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
